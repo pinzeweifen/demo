a3893c6eb5e837b1af8497ba38175c59348cda5b
pinzeweifen <719549260@qq.com>
2017-12-03 22:56:06 +0800

    -

---
+++ Unity/QEditorEvent.cs
@@ -0,0 +1,210 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public enum Mouse
+{
+    Left,
+    Middle,
+    Right
+}
+
+public class QEditorEvent
+{
+
+    #region public function
+
+    public static Vector2 MousePosition()
+    {
+        return Event.current.mousePosition;
+    }
+
+    public static void CommandPaste(Action callback)
+    {
+        Command("Paste", callback);
+    }
+
+    public static void CommandCopy(Action callback)
+    {
+        Command("Copy", callback);
+    }
+
+    public static void CommandCut(Action callback)
+    {
+        Command("Cut", callback);
+    }
+
+    public static void CommandDelete(Action callback)
+    {
+        Command("Delete", callback);
+    }
+
+    public static void CommandFrameSelected(Action callback)
+    {
+        Command("FrameSelected", callback);
+    }
+
+    public static void CommandDuplicate(Action callback)
+    {
+        Command("Duplicate", callback);
+    }
+
+    public static void CommandSelectAll(Action callback)
+    {
+        Command("SelectAll", callback);
+    }
+
+    public static bool IsKeyDown()
+    {
+        return Event.current.type == EventType.keyDown;
+    }
+
+    public static bool IsKeyUp()
+    {
+        return Event.current.type == EventType.KeyUp;
+    }
+
+    public static bool IsDoubleClick()
+    {
+        return Event.current.clickCount == 2;
+    }
+
+    public static bool IsContextClick()
+    {
+        return Event.current.type == EventType.ContextClick;
+    }
+
+    public static bool IsMouseDown()
+    {
+        return Event.current.type == EventType.mouseDown;
+    }
+
+    public static bool IsMouseUp()
+    {
+        return Event.current.type == EventType.mouseUp;
+    }
+
+    public static bool IsMouseMove()
+    {
+        return Event.current.type == EventType.mouseMove;
+    }
+
+    public static bool IsMouseDrag()
+    {
+        return Event.current.type == EventType.mouseDrag;
+    }
+
+    public static bool IsDragPerform()
+    {
+        if (Event.current.type == EventType.DragPerform
+          || Event.current.type == EventType.DragUpdated)
+        {
+            DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+            if (DragAndDrop.paths != null && DragAndDrop.paths.Length > 0)
+                return true;
+        }
+
+        return false;
+    }
+
+    public static bool IsDragEnd()
+    {
+        return Event.current.type == EventType.DragExited;
+    }
+
+    public static bool IsLayout()
+    {
+        return Event.current.type == EventType.Layout;
+    }
+
+    public static bool IsDoubleClick(Rect rect)
+    {
+        if (!IsDoubleClick()) return false;
+        return IsRectContains(rect);
+    }
+
+    public static bool IsContextClick(Rect rect)
+    {
+        if (!IsContextClick()) return false;
+        return IsRectContains(rect);
+    }
+
+    public static bool IsMouseDown(Rect rect)
+    {
+        if (!IsMouseDown()) return false;
+        return IsRectContains(rect);
+    }
+
+    public static bool IsMouseUp(Rect rect)
+    {
+        if (!IsMouseUp()) return false;
+        return IsRectContains(rect);
+    }
+
+    public static bool IsMouseMove(Rect rect)
+    {
+        if (!IsMouseMove()) return false;
+        return IsRectContains(rect);
+    }
+
+    public static bool IsDragPerform(Rect rect)
+    {
+        if ((Event.current.type == EventType.DragPerform
+          || Event.current.type == EventType.DragUpdated)
+          && rect.Contains(Event.current.mousePosition))
+        {
+            DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+            if (DragAndDrop.paths != null && DragAndDrop.paths.Length > 0)
+                return true;
+        }
+        return false;
+    }
+
+    public static bool IsDragEnd(Rect rect)
+    {
+        if (!IsDragEnd()) return false;
+        return IsRectContains(rect);
+    }
+
+    public static float GetScrollDelta()
+    {
+        return Event.current.delta.y;
+    }
+
+    public static KeyCode GetKeyCode()
+    {
+        return Event.current.keyCode;
+    }
+
+    public static Mouse GetButton()
+    {
+        return (Mouse)Event.current.button;
+    }
+
+    public static Vector2 GetMousePosition()
+    {
+        return Event.current.mousePosition;
+    }
+
+    #endregion
+
+    #region private function
+
+    private static bool IsRectContains(Rect rect)
+    {
+        return rect.Contains(Event.current.mousePosition);
+    }
+
+    private static void Command(string command, Action callback)
+    {
+        if (null == callback) return;
+        if (Event.current.type == EventType.ValidateCommand && Event.current.commandName == command)
+        {
+            callback();
+            Event.current.Use();
+        }
+    }
+
+
+    #endregion
+}
---
+++ Unity/QEditorLayout.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+
+public class QEditorLayout {
+
+    static Rect rect;
+    public static Rect Horizontal(Action<Rect> function, GUIStyle style,  params GUILayoutOption[] option)
+    {
+        rect = EditorGUILayout.BeginHorizontal(style, option);
+        function.Invoke(rect);
+        EditorGUILayout.EndHorizontal();
+        return rect;
+    }
+
+    public static Rect Horizontal(Action<Rect> function, params GUILayoutOption[] option)
+    {
+        rect = EditorGUILayout.BeginHorizontal(option);
+        function.Invoke(rect);
+        EditorGUILayout.EndHorizontal();
+        return rect;
+    }
+
+    public static Rect Vertical(Action<Rect> function, GUIStyle style,  params GUILayoutOption[] option)
+    {
+        rect = EditorGUILayout.BeginVertical(style, option);
+        function.Invoke(rect);
+        EditorGUILayout.BeginVertical();
+        return rect;
+    }
+
+    public static Rect Vertical(Action<Rect> function, params GUILayoutOption[] option)
+    {
+        rect = EditorGUILayout.BeginVertical(option);
+        function.Invoke(rect);
+        EditorGUILayout.BeginVertical();
+        return rect;
+    }
+
+    public static bool ToggleGroup(Action function,string label, bool toggle )
+    {
+        toggle = EditorGUILayout.BeginToggleGroup(label, toggle);
+        function.Invoke();
+        EditorGUILayout.EndToggleGroup();
+        return toggle;
+    }
+
+    public static Vector2 ScrollView(Action function, Vector2 scrollPosition,  GUIStyle style, params GUILayoutOption[] option)
+    {
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, style, option);
+        function.Invoke();
+        EditorGUILayout.EndScrollView();
+        return scrollPosition;
+    }
+
+    public static Vector2 ScrollView(Action function, Vector2 scrollPosition, params GUILayoutOption[] option)
+    {
+        scrollPosition = EditorGUILayout.BeginScrollView(scrollPosition, option);
+        function.Invoke();
+        EditorGUILayout.EndScrollView();
+        return scrollPosition;
+    }
+
+    private static bool isFadeGroup;
+    public static bool FadeGroup(Action function, float value)
+    {
+        isFadeGroup = EditorGUILayout.BeginFadeGroup(value);
+        function.Invoke();
+        EditorGUILayout.EndFadeGroup();
+        return isFadeGroup;
+    }
+
+}
