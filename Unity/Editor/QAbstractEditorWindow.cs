14cc35229b476671c105c011f16bf0162c3e53f4
pinzeweifen <719549260@qq.com>
2017-12-04 12:31:32 +0800

    -

---
+++ Unity/Editor/QAbstractEditorWindow.cs
@@ -0,0 +1,14 @@
+﻿
+
+using UnityEditor;
+
+public abstract class QAbstractEditorWindow {
+    protected EditorWindow w;
+
+    public QAbstractEditorWindow(EditorWindow window)
+    {
+        w = window;
+    }
+
+    public abstract void Update();
+}
