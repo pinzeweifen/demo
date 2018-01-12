508f9ac783739803a6b6e1fce880d4cc696db92b
pinzeweifen <719549260@qq.com>
2018-01-13 02:24:45 +0800

    提示UI

---
+++ Unity/QGUI/UI/MessageBoxUI.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class MessageBoxUI : MonoBehaviour {
+
+    public enum StandardButton
+    {
+        OK,No,Cancel
+    }
+
+    public Text title;
+    public Text text;
+    public Button ok;
+    public Button no;
+    public Button cancel;
+
+
+    public string Content
+    {
+        get { return text.text; }
+        set { text.text = value; }
+    }
+
+    [System.Serializable]
+    public class ClickButton : UnityEvent<StandardButton> { }
+    public ClickButton clickButton = new ClickButton();
+
+    private void Awake()
+    {
+        if(ok!=null)
+            ok.onClick.AddListener(() => { clickButton.Invoke(StandardButton.OK); });
+
+        if (no != null)
+            no.onClick.AddListener(() => { clickButton.Invoke(StandardButton.No); });
+
+        if (cancel != null)
+            cancel.onClick.AddListener(() => { clickButton.Invoke(StandardButton.Cancel); });
+    }
+}
