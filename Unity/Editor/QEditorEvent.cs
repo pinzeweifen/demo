ed0bb89e53aeb77c8da3ebc1bf74712817913596
pinzeweifen <719549260@qq.com>
2018-02-08 14:21:30 +0800

    更新编辑器事件

--- Unity/Editor/QEditorEvent.cs
+++ Unity/Editor/QEditorEvent.cs
@@ -2,240 +2,355 @@
 using UnityEditor;
 using System;
 
-public enum Mouse
+namespace QGUI
 {
-    Left,
-    Middle,
-    Right
+    public enum MouseButton
+    {
+        Left=0,
+        Right,
+        Middle
+    }
+
+    public static class QEditorEvent
+    {
+        #region public function
+        
+        /// <summary>
+        /// 粘贴命令
+        /// </summary>
+        /// <param name="callback"></param>
+        public static void CommandPaste(this Event current, Action callback)
+        {
+            current.Command("Paste", callback);
+        }
+
+        /// <summary>
+        /// 复制命令
+        /// </summary>
+        /// <param name="callback"></param>
+        public static void CommandCopy(this Event current, Action callback)
+        {
+            current.Command("Copy", callback);
+        }
+
+        /// <summary>
+        /// 剪切命令
+        /// </summary>
+        /// <param name="callback"></param>
+        public static void CommandCut(this Event current, Action callback)
+        {
+            current.Command("Cut", callback);
+        }
+
+        /// <summary>
+        /// 删除命令
+        /// </summary>
+        /// <param name="callback"></param>
+        public static void CommandDelete(this Event current, Action callback)
+        {
+            current.Command("Delete", callback);
+        }
+
+        /// <summary>
+        /// 定位到选定目标物体命令
+        /// </summary>
+        /// <param name="callback"></param>
+        public static void CommandFrameSelected(this Event current, Action callback)
+        {
+            current.Command("FrameSelected", callback);
+        }
+
+        /// <summary>
+        /// 克隆命令
+        /// </summary>
+        /// <param name="callback"></param>
+        public static void CommandDuplicate(this Event current, Action callback)
+        {
+            current.Command("Duplicate", callback);
+        }
+
+        /// <summary>
+        /// 选择所有命令
+        /// </summary>
+        /// <param name="callback"></param>
+        public static void CommandSelectAll(this Event current, Action callback)
+        {
+            current.Command("SelectAll", callback);
+        }
+
+        /// <summary>
+        /// 是否按下事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsKeyDown(this Event current)
+        {
+            return current.type == EventType.keyDown;
+        }
+
+        /// <summary>
+        /// 是否弹起事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsKeyUp(this Event current)
+        {
+            return current.type == EventType.KeyUp;
+        }
+
+        /// <summary>
+        /// 是否双击事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsDoubleClick(this Event current)
+        {
+            return current.clickCount == 2;
+        }
+
+        /// <summary>
+        /// 是否右键事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsContextClick(this Event current)
+        {
+            return current.type == EventType.ContextClick;
+        }
+
+        /// <summary>
+        /// 是否按下鼠标事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsMouseDown(this Event current)
+        {
+            return current.type == EventType.mouseDown;
+        }
+
+        /// <summary>
+        /// 是否弹起鼠标事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsMouseUp(this Event current)
+        {
+            return current.type == EventType.mouseUp;
+        }
+
+        /// <summary>
+        /// 是否移动鼠标事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsMouseMove(this Event current)
+        {
+            return current.type == EventType.mouseMove;
+        }
+
+        /// <summary>
+        /// 是否滚轮事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsScrollWhell(this Event current)
+        {
+            return current.type == EventType.ScrollWheel;
+        }
+
+        /// <summary>
+        /// 是否拖拽事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsDrag(this Event current)
+        {
+            return current.type == EventType.mouseDrag;
+        }
+
+        /// <summary>
+        /// 是否拖放事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsDragPerform(this Event current)
+        {
+            if (current.type == EventType.DragPerform
+              || current.type == EventType.DragUpdated)
+            {
+                DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+                if (DragAndDrop.paths != null && DragAndDrop.paths.Length > 0)
+                    return true;
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// 是否拖拽结束事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsDragEnd(this Event current)
+        {
+            return current.type == EventType.DragExited;
+        }
+
+        /// <summary>
+        /// 是否布局事件
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsLayout(this Event current)
+        {
+            return current.type == EventType.Layout;
+        }
+
+        /// <summary>
+        /// 是否在矩形内双击事件
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static bool IsDoubleClick(this Event current, Rect rect)
+        {
+            if (!current.IsDoubleClick()) return false;
+            return current.IsRectContains(rect);
+        }
+
+        /// <summary>
+        /// 是否在矩形内按下事件
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static bool IsContextClick(this Event current, Rect rect)
+        {
+            if (!current.IsContextClick()) return false;
+            return current.IsRectContains(rect);
+        }
+
+        /// <summary>
+        /// 是否在矩形内拖拽事件
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static bool IsMouseDrag(this Event current, Rect rect)
+        {
+            if (!current.IsDrag()) return false;
+            return current.IsRectContains(rect);
+        }
+
+        /// <summary>
+        /// 是否在矩形内按下鼠标事件
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static bool IsMouseDown(this Event current, Rect rect)
+        {
+            if (!current.IsMouseDown()) return false;
+            return current.IsRectContains(rect);
+        }
+
+        /// <summary>
+        /// 是否在矩形内弹起鼠标事件
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static bool IsMouseUp(this Event current, Rect rect)
+        {
+            if (!current.IsMouseUp()) return false;
+            return current.IsRectContains(rect);
+        }
+
+        /// <summary>
+        /// 是否在矩形内移动鼠标事件
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static bool IsMouseMove(this Event current, Rect rect)
+        {
+            if (!current.IsMouseMove()) return false;
+            return current.IsRectContains(rect);
+        }
+
+        /// <summary>
+        /// 是否在矩形内滚动事件
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static bool IsScrollWhell(this Event current, Rect rect)
+        {
+            if (!current.IsScrollWhell()) return false;
+            return current.IsRectContains(rect);
+        }
+
+        /// <summary>
+        /// 是否在矩形内拖放事件
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static bool IsDragPerform(this Event current, Rect rect)
+        {
+            if ((current.type == EventType.DragPerform
+              || current.type == EventType.DragUpdated)
+              && current.IsRectContains(rect))
+            {
+                DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
+                if (DragAndDrop.paths != null && DragAndDrop.paths.Length > 0)
+                    return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 是否在矩形内结束拖拽事件
+        /// </summary>
+        /// <param name="rect"></param>
+        /// <returns></returns>
+        public static bool IsDragEnd(this Event current, Rect rect)
+        {
+            if (!current.IsDragEnd()) return false;
+            return current.IsRectContains(rect);
+        }
+
+        /// <summary>
+        /// 获得滚动值
+        /// </summary>
+        /// <returns></returns>
+        public static float GetScrollDelta(this Event current)
+        {
+            return current.delta.y;
+        }
+
+        /// <summary>
+        /// 是否按下 Key 事件
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool IsKeyCode(this Event current,KeyCode key)
+        {
+            return current.keyCode == key;
+        }
+
+        /// <summary>
+        /// 是否按下鼠标按钮
+        /// </summary>
+        /// <param name="mouse"></param>
+        /// <returns></returns>
+        public static bool IsButton(this Event current,MouseButton button)
+        {
+            return current.button == (int)button;
+        }
+        
+        #endregion
+
+        #region private function
+
+        private static bool IsRectContains(this Event current, Rect rect)
+        {
+            return rect.Contains(current.mousePosition);
+        }
+
+        private static void Command(this Event current, string command, Action callback)
+        {
+            if (null == callback) return;
+            if (current.type == EventType.ValidateCommand && current.commandName == command)
+            {
+                callback();
+                current.Use();
+            }
+        }
+
+
+        #endregion
+    }
+
 }
 
-public class QEditorEvent
-{
-    #region public function
-
-    public static void Use()
-    {
-        Event.current.Use();
-    }
-
-    public static Vector2 Dalta()
-    {
-        return Event.current.delta;
-    }
-
-    public static bool ScrollWhellDalta()
-    {
-        return Dalta().y > 0 ? true : false;
-    }
-
-    public static Vector2 MousePosition()
-    {
-        return Event.current.mousePosition;
-    }
-
-    public static void CommandPaste(Action callback)
-    {
-        Command("Paste", callback);
-    }
-
-    public static void CommandCopy(Action callback)
-    {
-        Command("Copy", callback);
-    }
-
-    public static void CommandCut(Action callback)
-    {
-        Command("Cut", callback);
-    }
-
-    public static void CommandDelete(Action callback)
-    {
-        Command("Delete", callback);
-    }
-
-    public static void CommandFrameSelected(Action callback)
-    {
-        Command("FrameSelected", callback);
-    }
-
-    public static void CommandDuplicate(Action callback)
-    {
-        Command("Duplicate", callback);
-    }
-
-    public static void CommandSelectAll(Action callback)
-    {
-        Command("SelectAll", callback);
-    }
-
-    public static bool IsKeyDown()
-    {
-        return Event.current.type == EventType.keyDown;
-    }
-    
-    public static bool IsKeyUp()
-    {
-        return Event.current.type == EventType.KeyUp;
-    }
-
-    public static bool IsDoubleClick()
-    {
-        return Event.current.clickCount == 2;
-    }
-    
-    public static bool IsContextClick()
-    {
-        return Event.current.type == EventType.ContextClick;
-    }
-
-    public static bool IsMouseDown()
-    {
-        return Event.current.type == EventType.mouseDown;
-    }
-
-    public static bool IsMouseUp()
-    {
-        return Event.current.type == EventType.mouseUp;
-    }
-
-    public static bool IsMouseMove()
-    {
-        return Event.current.type == EventType.mouseMove;
-    }
-
-    public static bool IsScrollWhell()
-    {
-        return Event.current.type == EventType.ScrollWheel;
-    }
-
-    public static bool IsMouseDrag()
-    {
-        return Event.current.type == EventType.mouseDrag;
-    }
-
-    public static bool IsDragPerform()
-    {
-        if (Event.current.type == EventType.DragPerform
-          || Event.current.type == EventType.DragUpdated)
-        {
-            DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
-            if (DragAndDrop.paths != null && DragAndDrop.paths.Length > 0)
-                return true;
-        }
-
-        return false;
-    }
-
-    public static bool IsDragEnd()
-    {
-        return Event.current.type == EventType.DragExited;
-    }
-
-    public static bool IsLayout()
-    {
-        return Event.current.type == EventType.Layout;
-    }
-
-    public static bool IsDoubleClick(Rect rect)
-    {
-        if (!IsDoubleClick()) return false;
-        return IsRectContains(rect);
-    }
-
-    public static bool IsContextClick(Rect rect)
-    {
-        if (!IsContextClick()) return false;
-        return IsRectContains(rect);
-    }
-
-    public static bool IsMouseDrag(Rect rect)
-    {
-        if (!IsMouseDrag()) return false;
-        return IsRectContains(rect);
-    }
-
-    public static bool IsMouseDown(Rect rect)
-    {
-        if (!IsMouseDown()) return false;
-        return IsRectContains(rect);
-    }
-
-    public static bool IsMouseUp(Rect rect)
-    {
-        if (!IsMouseUp()) return false;
-        return IsRectContains(rect);
-    }
-
-    public static bool IsMouseMove(Rect rect)
-    {
-        if (!IsMouseMove()) return false;
-        return IsRectContains(rect);
-    }
-
-    public static bool IsScrollWhell(Rect rect)
-    {
-        if (!IsScrollWhell()) return false;
-        return IsRectContains(rect);
-    }
-    
-    public static bool IsDragPerform(Rect rect)
-    {
-        if ((Event.current.type == EventType.DragPerform
-          || Event.current.type == EventType.DragUpdated)
-          && rect.Contains(Event.current.mousePosition))
-        {
-            DragAndDrop.visualMode = DragAndDropVisualMode.Generic;
-            if (DragAndDrop.paths != null && DragAndDrop.paths.Length > 0)
-                return true;
-        }
-        return false;
-    }
-
-    public static bool IsDragEnd(Rect rect)
-    {
-        if (!IsDragEnd()) return false;
-        return IsRectContains(rect);
-    }
-
-    public static float GetScrollDelta()
-    {
-        return Event.current.delta.y;
-    }
-
-    public static bool GetKeyCode(KeyCode key)
-    {
-        return Event.current.keyCode == key;
-    }
-
-    public static Mouse GetButton()
-    {
-        return (Mouse)Event.current.button;
-    }
-
-    public static Vector2 GetMousePosition()
-    {
-        return Event.current.mousePosition;
-    }
-
-    #endregion
-
-    #region private function
-
-    private static bool IsRectContains(Rect rect)
-    {
-        return rect.Contains(Event.current.mousePosition);
-    }
-
-    private static void Command(string command, Action callback)
-    {
-        if (null == callback) return;
-        if (Event.current.type == EventType.ValidateCommand && Event.current.commandName == command)
-        {
-            callback();
-            Event.current.Use();
-        }
-    }
-
-
-    #endregion
-}
